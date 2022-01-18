48c21c132c5509959998e1d6a0a5209411c48ab2
alexwill44 <AlArvanitis@gmail.com>
2022-01-17 18:14:15 -0800

    randomly generates a flachcard

--- client/flashcards/src/components/Library/Gallery.jsx
+++ client/flashcards/src/components/Library/Gallery.jsx
@@ -3,14 +3,15 @@ import Card from './Card';
 
 function Gallery(props) {
 
-    const allCards = flashcards => {
-        return flashcards.map(flashcard => <Card {...flashcard} key={props.question + props.answer} />);
+    const randomCard = flashcards => {
+
+        return (<Card {...flashcards[Math.floor(Math.random()*flashcards.length)]} key={props.question + props.answer} />);
+        
     };
 
     return(
         <div className='gallery'>
-            <h3>Is this an array or an obj::{props.data.length}</h3>
-            <div className='cards'> {allCards(props.data)} </div>
+            <div className='cards'> {randomCard(props.data)} </div>
         </div>   
     );
 };
